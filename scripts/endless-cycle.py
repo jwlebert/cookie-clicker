8ddbf4717ad5aa4b9efc34f8fde517f43d7a74fd
jwlebert <37757770+jwlebert@users.noreply.github.com>
2026-08-22 17:54:20 -0400

    feat: added keyboard recognition, escape, and toggle

--- scripts/endless-cycle.py
+++ scripts/endless-cycle.py
@@ -5,7 +5,24 @@
 # ]
 # ///
 
+from pynput import keyboard
 import pyautogui
-import pynput
+import time
+import os
 
-print(pyautogui, pynput)
+pyautogui.FAILSAFE = True
+
+active = False
+def on_press(key):
+    global active
+
+    if key == keyboard.Key.esc:
+        print("Escape: exiting script.")
+        os._exit(0)
+
+    try:
+        if key.char == 'q':
+            print(f"Toggle: switching to {not active}")
+            active = not active
+    except AttributeError:
+        pass
